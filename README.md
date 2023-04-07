b435492ea920a141cdcb8be7fb2d7e45e4a4489b
Camille Viot <viot.camille@outlook.com>
2023-04-07 15:58:04 +0200

    up

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ openocd -f interface/stlink-v2-1.cfg -f target/stm32f3x.cfg
 run
 
 ````
-cargo run
+cargo run --release
 ````
 
 If needed change gdb init sequence in ``openocd.gdb``.
--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,19 @@ fn main() -> ! {
     let init_struct = aux5::init();
     let mut leds = init_struct.leds;
     let mut delay = init_struct.delay;
-    let button_a0 = init_struct.button_a0;
+    //let button_a0 = init_struct.button_a0;
     let button_d3 = init_struct.pd3_pin;
     let mut analog_input_d14 = init_struct.pd14_pin;
     let mut adc3: Adc<ADC3> = init_struct.adc3;
 
     loop {
-        // let button_state = button_d3.is_high().unwrap();
+        let button_state = button_d3.is_high().unwrap();
 
-        // if button_state {
-        //     leds[5].on().ok();
-        // } else {
-        //     leds[5].off().ok();
-        // }
+        if button_state {
+            leds[5].on().ok();
+        } else {
+            leds[5].off().ok();
+        }
 
         // let button_state = button_a0.is_active().unwrap();
 
@@ -40,14 +40,14 @@ fn main() -> ! {
 
         let scaled = adc_val_32 / 4095_f32;
 
-        for curr in 0..8 {
-            let current = (curr as f32) / 8_f32;
-            if scaled >  current {
-                leds[curr].on().ok();
-            } else {
-                leds[curr].off().ok();
-            }
-        }
+        // for curr in 0..8 {
+        //     let current = (curr as f32) / 8_f32;
+        //     if scaled >  current {
+        //         leds[curr].on().ok();
+        //     } else {
+        //         leds[curr].off().ok();
+        //     }
+        // }
 
         delay.delay_ms(10_u16);
     }
